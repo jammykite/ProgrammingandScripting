22bcff975aae80804b60a3802913a5c054aa657a
Jamie Irvine <50103233@belfastmet.ac.uk>
2024-10-24 16:22:32 +0100

    Week 2 classwork

---
+++ Jamies own projects/Text based adventure game.py
@@ -0,0 +1,6 @@
+# import random to allow CPU choice to be randomised
+import random
+
+# print game title
+print('Rock! Paper! Scissors!')
+print('New Game')
--- Week 2 - Conditional Statements/Exercise 3 Students Grades.py
+++ Week 2 - Conditional Statements/Exercise 3 Students Grades.py
@@ -1,6 +1,9 @@
+#while true creates a loop
 while True:
+    #Request that user enter their test score
  test_score=int(input('Please enter your test score 0 - 100: '))
 
+#define grades based on users input
  if (test_score <=100 and test_score >=90):
     print('Your grade is an A, Well done!')
  elif 80 <= test_score < 90:
@@ -11,5 +14,6 @@ while True:
     print('Your grade is a D, Keep trying!')
  elif 0 <= test_score < 60:
     print('Your grade is an F, Don’t give up!')
+    #user input error
  else:
     print('Invalid score. Please enter a score between 0 and 100.')
