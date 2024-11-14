af6faa846341981537bd78b550c1bd54da33534c
Jammykite <50103233@belfastmet.ac.uk>
2024-11-14 14:58:04 +0000

    Week 4

---
+++ Week 4 - Loops/Loop_break.py
@@ -0,0 +1,8 @@
+while True:
+    StudentNumber = input('Enter your Student Number: ')
+    if StudentNumber == '50103233':
+        print(StudentNumber, 'Student found')
+        break
+    else:
+        print(StudentNumber, 'not found')
+    print('Student Number not found')
