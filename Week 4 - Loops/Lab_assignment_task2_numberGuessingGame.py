0b312a3482d17d000ac94eea5f5e506aae2a82d8
Jamie Irvine <50103233@belfastmet.ac.uk>
2024-11-14 16:11:53 +0000

    Week 4 loops

---
+++ Week 4 - Loops/Lab_assignment_task2_numberGuessingGame.py
@@ -0,0 +1,12 @@
+"""Write a program that simulates a number guessing game.
+
+The program should pick a random number between 1 and 10.
+
+The user has five attempts to guess the number.
+
+If the user guesses correctly, print "Congratulations! You guessed it right!".
+
+If the guess is too high or too low, give a hint.
+
+End the game early if the user guesses correctly or runs out of attempts."""
+
