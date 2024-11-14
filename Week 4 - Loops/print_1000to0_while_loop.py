68f8303604d54dbdeaacccd74a410a22b6ba00a0
Jammykite <50103233@belfastmet.ac.uk>
2024-11-14 14:51:06 +0000

    Week 4

---
+++ Week 4 - Loops/print_1000to0_while_loop.py
@@ -0,0 +1,8 @@
+#define number (n)
+n = 1000
+#while number is greater than 0, print number
+while n>0:
+    print (n)
+#number minus 1, keep printing until no longer greater than 0
+    n = n - 1
+print('Done')
