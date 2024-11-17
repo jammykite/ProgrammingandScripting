ba8c41607130baf77145232815500499877e46b9
Jammykite <50103233@belfastmet.ac.uk>
2024-11-17 22:41:05 +0000

    Week 4

---
+++ Programming_and_scripting_coursework1/Task_Two.py
@@ -0,0 +1,16 @@
+"""
+Your second task is to build a chatbot for MoodSense, a company that offers mental wellness solutions.
+You need to develop a chatbot that asks users to describe their mood on a scale of 1 to 10 and provides
+an appropriate response based on their input.
+
+The response flow should follow this logic:
+•	1-3: “I’m sorry you’re feeling this way. Consider talking to a friend or taking some time for yourself.”
+•	4-7: “It seems like you're having an okay day. Remember to take breaks and focus on self-care.”
+•	8-10: “That’s great to hear! Keep up the positive energy!”
+"""
+
+"""
+Step One: Ask user to input an integer from 1-10
+Step Two: Determine output message based on users input
+Step Three: Print output
+"""
