b83c1cc1332b8ad637c41968d2f0e9bec6122e26
Jammykite <50103233@belfastmet.ac.uk>
2024-12-05 14:32:18 +0000

    Week 7, Dictionaries

--- Week_7_Dictionaries/Exercise1.py
+++ Week_7_Dictionaries/Exercise1.py
@@ -2,4 +2,15 @@
 
 Dom={True:[1234,1121], False:{'s':[12,22,12,9]}, 'colleges':['BelfastMet','QUB','Ulster Uni'], 0.245:'This is a float'}
 
+print(Dom['colleges'])
+
 print(Dom['colleges'][2])
+
+"""print 22 from above dictionary"""
+
+print(Dom[False]['s'][1])
+
+"""Print all keys and values"""
+
+print(Dom.keys())
+print(Dom.values())
