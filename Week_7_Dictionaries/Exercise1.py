4b168471ab7597444528ccc91f94af580ddcbce6
Jammykite <50103233@belfastmet.ac.uk>
2024-12-05 14:20:34 +0000

    Week 7, Dictionaries

---
+++ Week_7_Dictionaries/Exercise1.py
@@ -0,0 +1,5 @@
+"""Print Ulster Uni from below dictionary"""
+
+Dom={True:[1234,1121], False:{'s':[12,22,12,9]}, 'colleges':['BelfastMet','QUB','Ulster Uni'], 0.245:'This is a float'}
+
+print(Dom['colleges'][2])
