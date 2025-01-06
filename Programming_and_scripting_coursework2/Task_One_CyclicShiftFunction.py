bcaa817603467c2ca523d4fd4ffc2ee4c61a15f0
Jammykite <50103233@belfastmet.ac.uk>
2025-01-06 22:00:05 +0000

    Coursework 2

--- Programming_and_scripting_coursework2/Task_One_CyclicShiftFunction.py
+++ Programming_and_scripting_coursework2/Task_One_CyclicShiftFunction.py
@@ -22,14 +22,15 @@ Step One: Define a function that can process a range of datatypes (polymorphic).
 Step Two: Function should accept two inputs -
                                         m = the message to be shifted
                                         shift = the number of places to be shifted
-Step Three: Validation. If shift is negative or greater or equal to the length of m then the function should return
+Step Three: Validation. If shift is negative, greater or equal to the length of m then the function should return
 the original message. Input should also be a string or list
-Step Four: Calculate the effective shift 
+Step Four: Calculate the effective shift, this is done using the % operator to find the remainder if shift value is
+longer than message.
 Step Five: Perform the shift
 Step Six: Print the new shifted message 
 """
 
-
+#def to create a new function
 def cyclic_shift(m, shift):
     # Check if the input is valid (string or list) by using isinstance function
     if not isinstance(m, (str, list)):
@@ -46,12 +47,13 @@ def cyclic_shift(m, shift):
     if shift < 0 or shift >= mlength:
         return m  # Return the original message if shift is invalid.
 
-    # Calculate the effective shift (to handle shifts larger than the message length)
+    # Calculate the effective shift (this helps to handle shifts larger than the message length)
     effective_shift = shift % mlength  # This makes sure the shift wraps around correctly.
 
-    # Perform the cyclic shift - This creates a new version of m by combining two parts:
-    # 1 - The part of the input that is to be shifted
-    # 2 - The remainder of the input, starting from the beginning up to where the first part ends.
+    # Perform the cyclic shift - This creates a new version of m by slicing m according to the effective shift
+    # and combining the two parts:
+      # 1 - The part of the input that is to be shifted goes to the start
+      # 2 - The remainder of the input goes to the end
     shifted_m = m[-effective_shift:] + m[:-effective_shift]
 
     # Step 7: Return the shifted message
